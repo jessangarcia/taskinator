649f068157c1096a4191534a892521e1560dffda
jessan <jessenia.garcia998@gmail.com>
2021-07-08 23:03:52 -0600

    form sumbit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -9,5 +11,5 @@ var createTaskHandler = function() {
 }
 
 //Create li element 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
